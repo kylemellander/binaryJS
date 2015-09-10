e678e6bf236a3e231fd2e51dde8d63913c439c83
Kyle Mellander <student@epicodus.com>
2015-09-10 15:36:19 -0700

    Returns binary of 1

--- js/binary.js
+++ js/binary.js
@@ -2,7 +2,10 @@ function binary(number) {
   var remainder = number;
   var result = 0;
   for(var i = 1; remainder > 0; ++i) {
-
+    if(remainder % 2 !== 0) {
+      result += 1;
+      remainder -= 1;
+    }
   }
   return result;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,12 @@ describe("binary", function() {
     expect(binary(0)).to.equal(0)
   })
 
+  it("returns 1 when 1 is entered", function() {
+    expect(binary(1)).to.equal(1)
+  })
+
+  it("returns 10 when 2 is entered", function() {
+    expect(binary(2)).to.equal(10)
+  })
+
 })
