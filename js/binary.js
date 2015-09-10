74728fb38db9c993ec099e9213f0ea12ff19e69d
Kyle Mellander <student@epicodus.com>
2015-09-10 15:33:48 -0700

    Returns binary of 0

---
+++ js/binary.js
@@ -0,0 +1,8 @@
+function binary(number) {
+  var remainder = number;
+  var result = 0;
+  for(var i = 1; remainder > 0; ++i) {
+
+  }
+  return result;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,7 @@
-describe("wordUp", function() {
+describe("binary", function() {
+
+  it("returns 0 when 0 is entered", function() {
+    expect(binary(0)).to.equal(0)
+  })
 
 })
