bb7cc87c23476856e31939e5bfba7cd10cd29ec1
Kyle Mellander <student@epicodus.com>
2015-09-10 16:19:31 -0700

    Write function for converting any type of number

--- js/binary.js
+++ js/binary.js
@@ -2,10 +2,29 @@ function binary(number) {
   var remainder = number;
   var result = 0;
   for(var i = 1; remainder > 0; ++i) {
-    if(remainder % 2 !== 0) {
-      result += 1;
-      remainder -= 1;
+    if(remainder % Math.pow(2, i) !== 0) {
+      result += (Math.pow(10, i-1));
+      remainder -= Math.pow(2, i-1);
     }
   }
   return result;
+}
+
+function trinary(number) {
+  var remainder = number;
+  var result = 0;
+  for(var i = 1; remainder > 0; i) {
+    if(remainder % Math.pow(3, i) !== 0) {
+      result += (Math.pow(10, i-1));
+      remainder -= Math.pow(3, i-1);
+    } else {
+      i += 1;
+    }
+  }
+  return result;
+}
+
+function numberConverter(number, arg) {
+  return number.toString(arg);
+  alert("KYLE IS DUMB");
 }
--- spec/specs.js
+++ spec/specs.js
@@ -12,4 +12,48 @@ describe("binary", function() {
     expect(binary(2)).to.equal(10)
   })
 
+  it("returns large number result", function() {
+    expect(binary(2536)).to.equal(100111101000)
+  })
+
+})
+
+describe("trinary", function() {
+
+  it("returns 0 when 0 is entered", function() {
+    expect(trinary(0)).to.equal(0)
+  })
+
+  it("returns 1 when 1 is entered", function() {
+    expect(trinary(1)).to.equal(1)
+  })
+
+  it("returns 10 when 2 is entered", function() {
+    expect(trinary(2)).to.equal(2)
+  })
+
+  it("returns large number result", function() {
+    expect(trinary(236)).to.equal(22202)
+  })
+
+})
+
+describe("numberConverter", function() {
+
+  it("returns 0 when 0 is entered", function() {
+    expect(numberConverter(0, 16)).to.equal("0")
+  })
+
+  it("returns 1 when 1 is entered", function() {
+    expect(numberConverter(1, 16)).to.equal("1")
+  })
+
+  it("returns 10 when 2 is entered", function() {
+    expect(numberConverter(10, 16)).to.equal("a")
+  })
+
+  it("returns large number result", function() {
+    expect(numberConverter(236, 16)).to.equal("ec")
+  })
+
 })
